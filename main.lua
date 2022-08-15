ac13930f6d15011bbb86846be4155923ef07379a
zixtyy <111259955+zixtyy@users.noreply.github.com>
2022-08-15 09:40:10 +0700

    fix script for more complex!!

--- main.lua
+++ main.lua
@@ -1,3 +1,10 @@
 print('hello everyone!! i made my very first free fire hack real 2022!!!')
-while true do
+function push(p)
+   if p then
+    push(p)
+    end
+end
+
+while 64/32==2 do
+  push("p")
 end
